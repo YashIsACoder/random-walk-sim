ef9115442d0dc7570e70f144ead4cc0c2024b590
Yashwanth Madan <z5481579@ad.unsw.edu.au>
2025-12-14 20:09:21 +1100

    Added some basic tests for testing statistics functions to see if they work. Also coded up main to parse arguments from CLI

---
+++ build/CMakeFiles/random_walk.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/random_walk.dir/src/main.cpp.o"
+  "CMakeFiles/random_walk.dir/src/main.cpp.o.d"
+  "random_walk"
+  "random_walk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/random_walk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/randomwalk_lib.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/randomwalk_lib.dir/src/RandomWalk.cpp.o"
+  "CMakeFiles/randomwalk_lib.dir/src/RandomWalk.cpp.o.d"
+  "CMakeFiles/randomwalk_lib.dir/src/Statistics.cpp.o"
+  "CMakeFiles/randomwalk_lib.dir/src/Statistics.cpp.o.d"
+  "CMakeFiles/randomwalk_lib.dir/src/Walker.cpp.o"
+  "CMakeFiles/randomwalk_lib.dir/src/Walker.cpp.o.d"
+  "librandomwalk_lib.a"
+  "librandomwalk_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/randomwalk_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
